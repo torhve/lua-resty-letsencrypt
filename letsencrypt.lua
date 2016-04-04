torhve/lua-resty-letsencrypt

commit cb1be5f580e4bc6584b0dd3cee25884304c944f3

only reference dict name once

letsencrypt.lua
@@ -35,8 +35,8 @@ local http = require "resty.http" -- https://github.com/pintsized/lua-resty-http
 
 local _M = {}
 
--- NGINX shared worker memory. in nginx.conf:  lua_shared_dict acme 512k
-_M.ngx_mem = ngx.shared.acme
+-- NGINX shared worker memory. in nginx.conf:  lua_shared_dict acme 1m
+_M.cache = ngx.shared.acme
 
 local tableHasValue = function(table, value)
     if(type(table) ~= 'table') then return end
@@ -435,7 +435,7 @@ Lock.new = function(timeout)
         timeout = 10
     end
 
-    local dict = ngx.shared.acme
+    local dict = _M.cache
     if not dict then
         return nil, 'nginx shared dict not found'
     end
@@ -511,7 +511,7 @@ end
 
 -- A caching file loader
 file.load = function(fname)
-    local cache = ngx.shared.acme
+    local cache = _M.cache
     local key = '_fcache:'..fname
 
     local val, err = cache:get(key)
@@ -548,7 +548,7 @@ _M.new = function(conf)
     local lock = Lock.new()
     -- Flush caches, this makes it possible recheck cert files and reload files
     -- on nginx reload, since init_by_lua is ran.
-    ngx.shared.acme:flush_all()
+    _M.cache:flush_all()
     return setmetatable({
         conf = conf,
         account_file = account_file,

commit dc63f8ddab41dee54c1d3d449ac8415d82131f6d

Fix typo introduced in cb1be5f

letsencrypt.lua
@@ -682,7 +682,7 @@ _M.cert_for_host = function(self, host)
                     local challenge_test = http_challenge()
                     local key_authz = account.get_key_authz(challenge.token)
                     if challenge.type == "http-01" then
-                        self.ngx_mem:set('token:'..challenge.token, key_authz)
+                        self.cache:set('token:'..challenge.token, key_authz)
                     end
 
                     while challenge.status == "pending" and not challenge.keyAuthorization do
@@ -962,7 +962,7 @@ local challenge = function(self)
         ngx.exit(404)
     end
     log('Getting request for token: %s', token)
-    local authz = self.ngx_mem:get('token:'..token)
+    local authz = self.cache:get('token:'..token)
     if not authz then
         ngx.exit(404)
     else